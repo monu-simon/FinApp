07e6e5cca4da7acbed62dda47936f11a57279758
monu-simon <monusimonm@gmail.com>
2023-04-22 16:21:33 +0530

    Login Chnages

--- src/app/ui/general/general.module.ts
+++ src/app/ui/general/general.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common'
 import { HomeComponent } from './home/home.component'
 import { AboutComponent } from './about/about.component'
 import { RouterModule, Routes } from '@angular/router';
-import { WorkingComponent } from './working/working.component'
+import { WorkingComponent } from './working/working.component';
+import { LoginComponent } from './login/login.component'
 
 const routes: Routes = [
   { path: '', component: AboutComponent },
   { path: 'about', component: HomeComponent },
+  { path: 'login',component: LoginComponent}
 ]
 
 @NgModule({
-  declarations: [HomeComponent, AboutComponent, WorkingComponent],
+  declarations: [HomeComponent, AboutComponent, WorkingComponent, LoginComponent],
   imports: [CommonModule, RouterModule.forChild(routes)],
   exports: [HomeComponent, AboutComponent,RouterModule,WorkingComponent]
 })
