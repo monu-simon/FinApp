1e4913f92a796c66e0262655c1d3b414ffba4cd7
monu-simon <monusimonm@gmail.com>
2023-04-22 17:05:19 +0530

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './ui/shared/shared.module';
+
+
 
 @NgModule({
   declarations: [
