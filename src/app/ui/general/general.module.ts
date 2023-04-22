b47a57fa30179ad19c29f43e459989e7ee376421
monu-simon <monusimonm@gmail.com>
2023-04-22 15:03:55 +0530

    working component

--- src/app/ui/general/general.module.ts
+++ src/app/ui/general/general.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { HomeComponent } from './home/home.component'
 import { AboutComponent } from './about/about.component'
-import { RouterModule, Routes } from '@angular/router'
+import { RouterModule, Routes } from '@angular/router';
+import { WorkingComponent } from './working/working.component'
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent },
 ]
 
 @NgModule({
-  declarations: [HomeComponent, AboutComponent],
+  declarations: [HomeComponent, AboutComponent, WorkingComponent],
   imports: [CommonModule, RouterModule.forChild(routes)],
-  exports: [HomeComponent, AboutComponent,RouterModule]
+  exports: [HomeComponent, AboutComponent,RouterModule,WorkingComponent]
 })
 export class GeneralModule {}
