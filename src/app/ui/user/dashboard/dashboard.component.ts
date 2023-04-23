197ea08d61f1d554ac96ddff030821fd78fba7bb
monu-simon <monusimonm@gmail.com>
2023-04-23 17:32:12 +0530

    show data

--- src/app/ui/user/dashboard/dashboard.component.ts
+++ src/app/ui/user/dashboard/dashboard.component.ts
@@ -8,9 +8,10 @@ import { AngularFireAuth } from '@angular/fire/compat/auth'
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
   userId!: string | undefined
-  currentDate!: string;
+  currentDate!: string
+  amount!: number
+  response: any;
 
   constructor (
     private afAuth: AngularFireAuth,
@@ -22,11 +23,21 @@ export class DashboardComponent implements OnInit {
       this.userId = res?.uid
       //this.calculateService.createInitialExpenseEntry(this.userId,this.calculateService.getCurrentDate(),100);
       this.calculateService.getExpensesData(this.userId).then(res => {
-        console.log(res)
+        this.response = res;
       })
-      this.calculateService.isExpenseEntryExists(this.userId).then(res => console.log(res))
+      this.calculateService
+        .isExpenseEntryExists(this.userId)
+        .then(res => console.log(res))
     })
     console.log(this.calculateService.getCurrentDate())
+  }
+
+  create () {
+    console.log(this.amount);
+    this.afAuth.user.subscribe(res => {
+      this.userId = res?.uid;
+      this.calculateService.createInitialExpenseEntry(this.userId,this.calculateService.getCurrentDate(),this.amount);
+    })
     
   }
 }
