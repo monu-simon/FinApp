0ddc100e2fb45a531c90ab1ac842cc3b728189aa
monu-simon <monusimonm@gmail.com>
2023-04-28 20:17:27 +0530

    Logic cahnges

--- src/app/ui/user/view/view.component.ts
+++ src/app/ui/user/view/view.component.ts
@@ -3,6 +3,14 @@ import { CalculateService } from 'src/app/service/calculate.service'
 import { LoadingService } from 'src/app/service/loading.service'
 import { AngularFireAuth } from '@angular/fire/compat/auth'
 
+interface Transaction {
+  utility: string;
+  amount: number;
+}
+
+interface Transactions {
+  [date: string]: Transaction[];
+}
 @Component({
   selector: 'app-view',
   templateUrl: './view.component.html',
@@ -16,7 +24,7 @@ export class ViewComponent implements OnInit {
   ) {}
   userId!: string | undefined
   expenses: any
-
+  custom: any = {};
   ngOnInit (): void {
     this.afAuth.user.subscribe(res => {
       this.userId = res?.uid
@@ -25,6 +33,18 @@ export class ViewComponent implements OnInit {
       this.calculate.getExpensesData(this.userId).then(res => {
         this.expenses = res?.expensesData.expenses
         this.loading.stopLoading()
+        this.expenses.forEach((expense:any) => {
+          if(Object.keys(this.custom).includes(expense.date)) {
+            console.log(typeof(expense.date))
+            console.log((expense.amount[0]))
+            //this.custom[expense.date].push(JSON.parse(expense.amount[0]))
+          }else {
+            this.custom[expense.date] = Array.from([1,2]);
+            //this.custom[expense.date].push(JSON.parse(expense.amount[0]))
+          }
+        });
+        console.log(this.custom)
+        console.log(Object.entries(this.custom))
       })
     })
   }
